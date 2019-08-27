e4c2f12fe60f71b1394f3530848ddb3528012313
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-27 14:58:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,7 +4,7 @@ class School
     @roster = {}
   end
   def add_student(name, grade)
-    unless @roster[grade]
+    if @roster[grade] = nil
       @roster[grade] = []
     end
     @roster[grade].push(name)
